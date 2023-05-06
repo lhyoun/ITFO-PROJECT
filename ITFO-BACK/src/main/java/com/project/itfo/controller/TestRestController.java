1e4a229f1525a2c6f5b56aa8bcb380c63eb78395
DESKTOP-GAC14S5\jjdms <jjdmst@gmail.com>
2023-05-06 16:45:20 +0900

    Revert "그래들 db설정 수정, jpa 테스트 코드 추가"
    This reverts commit b012217d33db0c14f33ed7878c2bf92602dfd12c.

--- ITFO-BACK/src/main/java/com/project/itfo/controller/TestRestController.java
+++ ITFO-BACK/src/main/java/com/project/itfo/controller/TestRestController.java
@@ -1,31 +1,15 @@
 package com.project.itfo.controller;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.itfo.service.SysCodeService;
-import com.project.itfo.vo.SysCode;
-
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping("/test")
-@RequiredArgsConstructor
 public class TestRestController {
-	
-	private final SysCodeService sysCodeService;
 	
 	@GetMapping("/test1")
 	public String testController(){
         return "SUCCESS";
     }
-	
-	@GetMapping("/test2")
-	public List<SysCode> findAll(){
-		List<SysCode> sysList = this.sysCodeService.findAll();
-		return sysList;
-	}
 }
--- ITFO-BACK/src/main/java/com/project/itfo/repository/SysCodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.itfo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.itfo.vo.SysCode;
-
-public interface SysCodeRepository extends JpaRepository<SysCode, String>{
-
-}
