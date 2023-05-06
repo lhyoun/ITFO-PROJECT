9172fe54fb00f705f309bd11a29df023c0fe989f
lhy <gkdbsx@naver.com>
2023-05-06 15:14:05 +0900

    [setting] bootstrap

--- ITFO-FRONT/src/App.tsx
+++ ITFO-FRONT/src/App.tsx
@@ -2,13 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import "./App.css";
 import Header from "./modules/common/header/Header";
+import MainContainer from "./modules/main/MainContainer";
+import BoardContainer from "./modules/board/BoardContainer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <BrowserRouter>
-        <Routes>{/* <Route path="" element={<Test/>}></Route> */}</Routes>
+        <Routes>
+          <Route path="/" element={<MainContainer />}></Route>
+          <Route path="/board" element={<BoardContainer />}></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ ITFO-FRONT/src/modules/main/MainContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MainContainer = () => {
+    return (
+        <div>
+            aaa
+            <Link to="/board">board</Link>
+        </div>
+    );
+};
+
+export default MainContainer
